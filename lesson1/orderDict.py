9af340ef2be5a41317c8ce97e5630e4d00e5849d
Dod-Annie <936166900@qq.com>
2017-10-31 21:50:30 +0800

    add

---
+++ lesson1/orderDict.py
@@ -0,0 +1,32 @@
+# 让字典保持有序
+
+## 使用OderedDict
+
+from collections import OrderedDict
+
+d = OrderedDict()
+
+d['Jim'] = (1,35)
+
+d['Leo'] = (2,37)
+
+d['Bob'] = (3,40)
+
+for k in d:
+    print(k)
+
+from time import time
+from random import randint
+
+d = OrderedDict()
+players = list('ABCDEFGH')
+start = time()
+
+for i in range(8):
+    input()
+    p = players.pop(randint(0,7-i))
+    end = time()
+    print(i+1,p,end-start)
+    d[p] = (i+1,end-start)
+
+    
